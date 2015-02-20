cf02c236c7eabddeca80428e674b3ea9ab253613
Stephen Nguyen <nereuxie@gmail.com>
2015-02-20 13:49:36 -0700

    First Commit

---
+++ lab2p1.c
@@ -0,0 +1,29 @@
+// ******************************************************************************************* //
+// File:         lab2p1.c
+// Date:         
+// Authors:      
+//
+// Description: 
+// ******************************************************************************************* //
+
+#include "p24fj64ga002.h"
+#include "lcd.h"
+#include "timer.h"
+#include "keypad.h"
+#include <stdio.h>
+
+_CONFIG1( JTAGEN_OFF & GCP_OFF & GWRP_OFF & BKBUG_ON & COE_OFF & ICS_PGx1 &
+          FWDTEN_OFF & WINDIS_OFF & FWPSA_PR128 & WDTPS_PS32768 )
+
+_CONFIG2( IESO_OFF & SOSCSEL_SOSC & WUTSEL_LEG & FNOSC_PRIPLL & FCKSM_CSDCMD & OSCIOFNC_OFF &
+          IOL1WAY_OFF & I2C1SEL_PRI & POSCMOD_XT )
+
+int main(void)
+{
+
+    while(1){
+        
+    }
+    
+    return 0;
+}
